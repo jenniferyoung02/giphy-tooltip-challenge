844d4504cb729df4f1abc3592c28194c17d60d57
Jennifer Fox <jfox@ginkgobioworks.com>
2020-09-19 14:51:22 -0400

    Updates

---
+++ precache-manifest.0f77dfb36fb61da8963fd81a386b9e90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e932d39335db1a2f655909099775f1b1",
+    "url": "/giphy-tooltip-challenge/index.html"
+  },
+  {
+    "revision": "ccaba7d6d5b3bd1eeb22",
+    "url": "/giphy-tooltip-challenge/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "6b9cc60a73c3c80282e2",
+    "url": "/giphy-tooltip-challenge/static/css/main.df93cc5a.chunk.css"
+  },
+  {
+    "revision": "ccaba7d6d5b3bd1eeb22",
+    "url": "/giphy-tooltip-challenge/static/js/2.f9b16980.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/giphy-tooltip-challenge/static/js/2.f9b16980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b9cc60a73c3c80282e2",
+    "url": "/giphy-tooltip-challenge/static/js/main.0f9be14e.chunk.js"
+  },
+  {
+    "revision": "2ad580afd11ef4da7691",
+    "url": "/giphy-tooltip-challenge/static/js/runtime-main.8d1f99f7.js"
+  }
+]);
